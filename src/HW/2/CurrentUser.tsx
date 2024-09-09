cbdb3041773489ac9a69336b63de91e6e2aab9ee
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-09-09 13:52:01 +0300

    hw2

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,7 +1,7 @@
-import { UserType } from './HW2';
+
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: any
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
